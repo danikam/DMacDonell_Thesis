45c007fa65019cef78450e4e8e1a3b51074e72c7
damacdon <danikam1@uvic.ca>
2022-01-24 11:31:28 -0800

    Converted .jpg image to .pdf in ch 3

--- Figures/3/convert_to_pdf.sh
+++ Figures/3/convert_to_pdf.sh
@@ -2,3 +2,8 @@ for filename in *.png; do
   base_filename=${filename%%.*}
   convert -density 100 $filename ${base_filename}.pdf
 done
+
+for filename in *.jpg; do
+  base_filename=${filename%%.*}
+  convert -density 100 $filename ${base_filename}.pdf
+done
