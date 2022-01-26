2c77ac58bbe3cc8b090732c51cd5abd49e11e806
damacdon <danikam1@uvic.ca>
2022-01-26 15:45:37 -0800

    Added section on min(ms)

---
+++ Figures/5/download_plots.sh
@@ -0,0 +1,18 @@
+scp_vm docker_test_3:/mnt/Git/makehistos/Plots_presel_2jets/Baseline_gt2jets_ms/*.pdf .
+mv WCand_m.pdf WCand_m_ms.pdf
+mv TARJets10_minmS_res.pdf TARJets10_minmS_res_ms.pdf
+
+scp_vm docker_test_3:/mnt/Git/makehistos/Plots_presel_2jets/Baseline_gt2jets_mZp/*.pdf .
+mv TARJets10_minmS_res.pdf TARJets10_minmS_res_mZp.pdf
+mv WCand_m.pdf WCand_m_mZp.pdf
+
+scp_vm docker_test_3:/mnt/Git/makehistos/Plots_presel_1TARjet/Baseline_1TARjet_ms/*.pdf .
+mv TARJets10_mTAR0.pdf TARJets10_mTAR0_ms.pdf
+mv TARJets10_minmS_mgd.pdf TARJets10_minmS_mgd_ms.pdf
+
+scp_vm docker_test_3:/mnt/Git/makehistos/Plots_presel_1TARjet/Baseline_1TARjet_mZp/*.pdf .
+mv TARJets10_minmS_mgd.pdf TARJets10_minmS_mgd_mZp.pdf
+mv TARJets10_mTAR0.pdf TARJets10_mTAR0_mZp.pdf
+
+
+
