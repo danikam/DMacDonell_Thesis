be3abc5b2b17ce11c96ac90f1475fbd259b40f27
damacdon <danikam1@uvic.ca>
2022-01-26 21:53:19 -0800

    Added description of mT, and started on selections

--- Figures/5/download_plots.sh
+++ Figures/5/download_plots.sh
@@ -14,5 +14,5 @@ scp_vm docker_test_3:/mnt/Git/makehistos/Plots_presel_1TARjet/Baseline_1TARjet_m
 mv TARJets10_minmS_mgd.pdf TARJets10_minmS_mgd_mZp.pdf
 mv TARJets10_mTAR0.pdf TARJets10_mTAR0_mZp.pdf
 
-
+scp_vm docker_test_3:/mnt/Git/makehistos/Plots_mT_SWWlep008/SR1L_Presel/*.pdf .
 
