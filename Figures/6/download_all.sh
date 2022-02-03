c057ec2c64a16ec1c56a676856c4ff2cef900991
damacdon <danikam1@uvic.ca>
2022-02-02 23:31:11 -0800

    Updates to ch 6

---
+++ Figures/6/download_all.sh
@@ -0,0 +1,2 @@
+scp_vm docker_test_3:/mnt/Git/makehistos/Plots/exp_systs_total_bkg_SR_*_TARJets10_minmS_*.pdf .
+scp_vm docker_test_3:/mnt/Git/makehistos/Plots/exp_systs_monoSWWsemilep_zp2100_dm200_dh210_SR_*_TARJets10_minmS_*.pdf .
