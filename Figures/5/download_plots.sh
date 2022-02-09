29239ecac5d6c067c69f6bc26ef04f4b8e7a72b2
damacdon <danikam1@uvic.ca>
2022-02-08 22:40:49 -0800

    Updates to Ch. 5 to address feedback from Bob, and from proof-reading Ch. 7

--- Figures/5/download_plots.sh
+++ Figures/5/download_plots.sh
@@ -20,4 +20,8 @@ scp_vm -r docker_test_3:/mnt/Git/makehistos/Plots_N_1_significance/* .
 rm SR1L_Merged/*.png
 rm SR1L_Resolved/*.png
 
-scp_vm docker_test_3:/mnt/Git/makehistos/Plots_N_1_comparisons/CRW_Merged_metsig_gt_16/MetTST_met_N_1.pdf MetTST_met_N_1_CRW_metsig_gt_16.pdf 
+scp_vm docker_test_3:/mnt/Git/makehistos/Plots_N_1_comparisons/CRW_Merged_metsig_gt_16/MetTST_met_N_1.pdf MetTST_met_N_1_CRW_metsig_gt_16.pdf
+
+scp_vm docker_test_3:/mnt/Git/makehistos/Plots_presel_1TARjet/Baseline_1TARjet_e_zp1000_dm200_dh335/TARJets10_mTAR0.pdf zp1000_dm200_dh335_disentanglement.pdf
+
+scp_vm docker_test_3:/mnt/Git/makehistos/Plots_presel_1TARjet/Baseline_1TARjet_e_zp1000_dm200_dh260/TARJets10_mTAR0.pdf zp1000_dm200_dh260_disentanglement.pdf
