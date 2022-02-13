07906852cfee6b54795f54fa2b3e198286564619
damacdon <danikam1@uvic.ca>
2022-02-12 18:18:46 -0800

    Updates to ch 8

---
+++ Figures/8/download_all.sh
@@ -0,0 +1,3 @@
+scp_vm docker_test_3:/mnt/Git/histfitter-config/plots/unblinded_nosig.pdf .
+cp /Users/danikam/Git/darkhiggs_limits_combined/combined_contour.pdf .
+scp_vm docker_test_3:/mnt/Git/histfitter-config/results_nominal_bkgonly/MonoSlep_BkgOnly/*.pdf BkgOnly/
