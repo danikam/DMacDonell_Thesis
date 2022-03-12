30bbbb223d926728e8af2f0b6a60d4a983e67ea9
damacdon <danikam1@uvic.ca>
2022-03-12 14:57:47 -0800

    Added lists of figures and tables

--- Figures/8/download_all.sh
+++ Figures/8/download_all.sh
@@ -1,4 +1,4 @@
-#scp_vm docker_test_3:/mnt/Git/histfitter-config/plots/unblinded_nosig.pdf .
+scp_vm docker_test_3:/mnt/Git/histfitter-config/plots/unblinded_nosig.pdf .
 cp /Users/danikam/Git/darkhiggs_limits_combined/combined_contour.pdf .
 #scp_vm docker_test_3:/mnt/Git/histfitter-config/results_nominal_bkgonly/MonoSlep_BkgOnly/*.pdf BkgOnly/
 
